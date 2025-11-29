b2a88aa55f004e6906875973aaf2dc0f75d7ebc9
etike <etiketochukwu@gmail.com>
2025-11-29 17:28:55 +0000

    add category tags

---
+++ Migration.DbUp/Scripts/008_create_categories.sql
@@ -0,0 +1,7 @@
+INSERT INTO category (id, name) VALUES
+                                    ('0199d4ef-ca22-7970-a8d2-579818a5030d', 'Food and Resturants'),
+                                    ('0199d4ef-ca22-7970-a8d2-57995e57ebb3', 'Shopping'),
+                                    ('0199d4ef-ca22-7970-a8d2-57007439b317', 'Hotels,BnB and vacation'),
+                                    ('0199d4ef-ca22-7970-a8d2-570118a5030d', 'Fashion and Baauty'),
+                                    ('0199d4ef-ca22-7970-a8d2-57025e57ebb3', 'Health and Wellness'),
+                                    ('0199d4ef-ca22-7970-a8d2-57037439b317', 'Education and Training');
