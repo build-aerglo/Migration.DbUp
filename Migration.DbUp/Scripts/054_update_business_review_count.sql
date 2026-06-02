992db97d260086523fb5320fee85bea6fb4563f2
Anonymous_Coder <120263561+Gglovie@users.noreply.github.com>
2026-06-02 12:22:50 +0100

    final commit

--- Migration.DbUp/Scripts/054_update_business_review_count.sql
+++
@@ -1,8 +0,0 @@
-UPDATE business b
-SET review_count = (
-    SELECT COUNT(*)
-    FROM review r
-    WHERE r.business_id = b.id
-      AND r.status = 'APPROVED'
-      AND r.is_verification_pending = FALSE
-);
